c3aca8e08c055e3b2d22f0ad7085592fc0063d43
adii21-Ux <adityasangave2112@gmail.com>
2022-06-02 13:26:38 +0530

    Added token authntication

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', homepage_view, name="homepage"),
-    path('room', room_view, name="roomview")
+    path('room/', room_view, name="roomview"),
+    path('gettoken/', generateToken)
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,21 @@
+import random
+import time
 from django.shortcuts import render
+from django.http import JsonResponse
+from agora_token_builder import RtcTokenBuilder
 
-# Create your views here.
+def generateToken(request):
+    appID = "fba883be51524f7ca7dc32812b446325"
+    appCertificate = "5023805a4aaa4b8d88dc55a7e1f9c750"  
+    channelName = request.GET.get("channel")
+    uid = 0
+    role =1
+    privilegeExpiredTs = 3600 * 24 + time.time()
+    
+    token = RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channelName, uid, role, privilegeExpiredTs)
+    print(token)
+    return JsonResponse({'token':token, 'uid':uid}, safe=False)
+
 def homepage_view(request):
     return render(request, "core/homepage.html");
 
